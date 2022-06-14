3c1e976d719fa864efb45d237071ce8a52dce46a
versagar <85016589+versagar@users.noreply.github.com>
2022-06-14 15:53:25 +0530

    Add files via upload
    After deleting all old files, I added updated files.

---
+++ jscript.js
@@ -0,0 +1,23 @@
+function openNav() { 
+    document.getElementById("mySidenav").style.width = "40vw";
+    document.getElementById("mySidenav").style.backgroundColor="rgba(0,0,0,0.7)"
+    }
+    
+function closeNav() {document.getElementById("mySidenav").style.width="0";
+    }
+    
+function openOv() {
+      document.getElementById("myOv").style.width="20vw";
+      document.getElementById("myFeed").style.width="79vw";
+      document.getElementById("openOv").style.width="0"
+      document.getElementById("openOv").style.padding="0"
+      document.getElementById("info").style.width="75vw";
+    }
+function closeOv() {
+      document.getElementById("myOv").style.width = "0";
+      document.getElementById("myFeed").style.width="100vw";
+      document.getElementById("openOv").style.width="6vw";
+      document.getElementById("openOv").style.padding="10px";
+      document.getElementById("info").style.width="85vw";
+ 
+    }
