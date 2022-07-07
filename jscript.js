7f5c71315dd3d0fb9b4fa06136b54ec42778de4f
versagar <85016589+versagar@users.noreply.github.com>
2022-07-08 00:21:18 +0530

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -1,23 +1,25 @@
-function openNav() { 
-    document.getElementById("mySidenav").style.width = "40vw";
-    document.getElementById("mySidenav").style.backgroundColor="rgba(0,0,0,0.7)"
-    }
-    
-function closeNav() {document.getElementById("mySidenav").style.width="0";
-    }
-    
-function openOv() {
-      document.getElementById("myOv").style.width="20vw";
-      document.getElementById("myFeed").style.width="79vw";
-      document.getElementById("openOv").style.width="0"
-      document.getElementById("openOv").style.padding="0"
-      document.getElementById("info").style.width="75vw";
-    }
-function closeOv() {
-      document.getElementById("myOv").style.width = "0";
-      document.getElementById("myFeed").style.width="99vw";
-      document.getElementById("openOv").style.width="auto";
-      document.getElementById("openOv").style.padding="10px";
-      document.getElementById("info").style.width="85vw";
- 
-    }
+countOne= 0;
+countTwo= 0;
+vcounterOne= String('');
+vcounterTwo= String('');
+
+function incrfirst(){
+    countOne= countOne+ 1;
+    vcounterOne= vcounterOne+ String('🍎');
+    document.getElementById('firstNum').innerHTML= countOne;
+    document.getElementById('vcountOne').innerHTML= vcounterOne;
+}
+
+function incrsecond(){
+    countTwo= countTwo+ 1;
+    vcounterTwo= vcounterTwo+ String('🍓');
+    document.getElementById('secondNum').innerHTML= countTwo;
+    document.getElementById('vcountTwo').innerHTML= vcounterTwo;
+}
+
+function getsum(){
+    sum = countOne+ countTwo;
+    vsum= vcounterOne+ vcounterTwo;
+    document.getElementById('sumNum').innerHTML= sum;
+    document.getElementById('vSum').innerHTML= vsum;
+}
