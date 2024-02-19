9d27ef6196b856866e4e3dbdce8d4ed81091d1bf
versagar <85016589+versagar@users.noreply.github.com>
2024-02-19 18:40:30 +0530

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -23,11 +23,26 @@ function openForm() {
     if (a==0) {
         document.getElementById("form").style.display= "block";
         a = 1;
+        document.getElementById("form").src= "forms.google.com";
         document.getElementById("formBtn").innerHTML= "Close Form";
     } else {
         document.getElementById("form").style.display= "none";
         a = 0;
         document.getElementById("formBtn").innerHTML= "Open Form";
+    }
+
+}
+function openSagar() {
+    
+    if (a==0) {
+        document.getElementById("form").style.display= "block";
+        a = 1;
+        document.getElementById("form").src= "dtbs/saga&1214.html";
+        document.getElementById("sagarBtn").innerHTML= "Close Profile";
+    } else {
+        document.getElementById("form").style.display= "none";
+        a = 0;
+        document.getElementById("sagarBtn").innerHTML= "Open Profile";
     }
 
 }
